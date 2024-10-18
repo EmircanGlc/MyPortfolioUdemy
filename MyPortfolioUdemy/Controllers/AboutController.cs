b10b464281fa0d8185a32de5ad06321dd272665c
user <user@ALFA_3250_AIO>
2024-10-18 15:52:43 +0300

    Add project files.

---
+++ MyPortfolioUdemy/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.Controllers
+{
+	public class AboutController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult AboutText()
+		{
+			var values = context.Abouts.ToList();
+			return View(values);
+		}
+	}
+}
---
+++ MyPortfolioUdemy/Controllers/ExperienceController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class ExperienceController : Controller
+    {
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult ExperienceList()
+        {
+            
+            var values = context.Experiences.ToList(); 
+            return View(values);
+        }
+        //Yeni deneyim girişi yapmak için burayı kullanacağız.
+        [HttpGet] //Sayfa ilk yüklendiğinde açılacak.
+        public IActionResult CreateExperience()
+        {
+                return View();
+        }
+        [HttpPost] //Sayfada bir butona tıklandığında çalışacak.
+        public IActionResult CreateExperience(Experience experience) 
+        {
+			context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        public IActionResult DeleteExperience(int id) //Silme işlemi Id'ye göre yapılacak.
+        {
+            var value = context.Experiences.Find(id); //Silinecek veriyi bulduk. s.değer = value 
+            context.Experiences.Remove(value); //Valuedan gelen değeri sil.
+            context.SaveChanges(); //Kaydet
+			return RedirectToAction("ExperienceList"); //Bizi tekrar ExperienceListe yönlendir.
+		}
+        [HttpGet]
+        public IActionResult UpdateExperience(int id) 
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+
+            
+        }
+	}
+}
